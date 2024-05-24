d7be046959f54c8944cc89e02b895c023c4b76ef
Lunelli Riccardo <rikilnl@gmail.com>
2024-05-24 15:59:41 +0200

    wip

--- train_decoder.py
+++ train_decoder.py
@@ -9,22 +9,22 @@ from transformers import get_cosine_schedule_with_warmup
 import torch.nn as nn
 from dataset import get_datasets, RSICD_, UCM, NWPU, SIDNEY
 from model import ClipGPT, CLIP, REMOTE_CLIP, VGG
-from torchmetrics.text.bleu import BLEUScore
-from tqdm import tqdm
 import argparse
 from huggingface_hub import hf_hub_download
 from pycocoevalcap.spice.spice import Spice
+from pycocoevalcap.cider.cider import Cider
 import wandb
 from utils import collate_fn_train, collate_fn_val
 from torch.cuda.amp import GradScaler, autocast
+from tqdm import tqdm
 
 
 # Add argument parser for hyperparameters
 parser = argparse.ArgumentParser(description='Train Decoder')
 parser.add_argument('--device', type=str, default=None, help='Device to use for training')
 parser.add_argument('--epochs', type=int, default=15, help='Number of training epochs')
-parser.add_argument('--lr_gen', type=float, default=1e-4, help='Learning rate for generator')
-parser.add_argument('--lr_adapter', type=float, default=3e-4, help='Learning rate for adapted layer')
+parser.add_argument('--lr_gen', type=float, default=1e-5, help='Learning rate for generator')
+parser.add_argument('--lr_adapter', type=float, default=3e-5, help='Learning rate for adapted layer')
 parser.add_argument('--weight_decay', type=float, default=0., help='Weight decay for optimizer')
 parser.add_argument('--warmup_steps', type=float, default=200, help='Number of warmup ratio for scheduler')
 parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
@@ -33,7 +33,7 @@ parser.add_argument('--dropout', type=float, default=0.2, help='Dropout rate for
 parser.add_argument('--dataset', type=str, default=None, help='Dataset to use for training')
 parser.add_argument('--log', action='store_true', help='Log to wandb')
 parser.add_argument('--encoder', type=str, default=CLIP, help='Decoder model to use')
-parser.add_argument('--lr_encoder', type=float, default=1e-4, help='Learning rate for decoder')
+parser.add_argument('--lr_encoder', type=float, default=1e-5, help='Learning rate for decoder')
 args = parser.parse_args()
 
 assert args.encoder in [CLIP, REMOTE_CLIP, VGG]
@@ -53,26 +53,27 @@ dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle
 dataloader_val = DataLoader(dataset_val, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn_val)
  
 if args.encoder == VGG:
-    optimizer_gen = torch.optim.AdamW([
+    optimizer = torch.optim.AdamW([
         { 'params': net.generator.parameters(), 'lr': args.lr_gen},
         { 'params': net.adapted_layer.parameters(), 'lr': args.lr_adapter},
-        { 'params': net.vgg.parameters(), 'lr': args.lr_encoder}
+        { 'params': net.vgg.features.parameters(), 'lr': args.lr_encoder}
     ], weight_decay=args.weight_decay)
 else:
-    optimizer_gen = torch.optim.AdamW([
+    optimizer = torch.optim.AdamW([
         { 'params': net.generator.parameters(), 'lr': args.lr_gen},
         { 'params': net.adapted_layer.parameters(), 'lr': args.lr_adapter}
     ], weight_decay=args.weight_decay)
 
 sched_gpt = get_cosine_schedule_with_warmup(
-    optimizer_gen,
+    optimizer,
     num_warmup_steps=args.warmup_steps, 
     num_training_steps=len(dataloader_train) * args.epochs
 )
 
 # store best results
-best_spice = 0
+best_score = 0
 spice_scorer = Spice()
+cider_scorer = Cider()
 
 if device == 'cuda':
     scaler = GradScaler()
@@ -89,15 +90,15 @@ for epoch in train_pbar:
         with autocast():
             loss = net.train_generator(captions, images=images)
 
-                        # Backpropagation with gradient scaling
+            # Backpropagation with gradient scaling
             if device == 'cuda':
                 scaler.scale(loss).backward()
-                scaler.step(optimizer_gen)
+                scaler.step(optimizer)
                 scaler.update()
             else:
                 loss.backward()
-                optimizer_gen.step()
-                optimizer_gen.zero_grad()
+                optimizer.step()
+                optimizer.zero_grad()
             train_losses.append(loss.item())
             sched_gpt.step()
             epoch_bar.set_postfix(loss_gpt=loss.item())    
@@ -116,19 +117,22 @@ for epoch in train_pbar:
             for c in range(len(results)):
                 refs[count + c] = captions[c]
                 res[count + c] = [results[c]]
-
             count += len(results)
 
-        spice_score, _ = spice_scorer.compute_score(refs, res)
+        # non mi va spice sul mac
+        if device == 'mps':
+            score, _ = cider_scorer.compute_score(refs, res)
+        else:
+            score, _ = spice_scorer.compute_score(refs, res)
 
-        if spice_score > best_spice:
-            best_spice = spice_score
-            print(f'Saving model with SPICE score: {spice_score}')
+        if score > best_score:
+            best_score = score
+            print(f'Saving model with best score: {score}')
             torch.save(net.state_dict(), 'data/models/full.pth')
 
-    train_pbar.set_postfix(train_loss_gpt=np.mean(train_losses), val_spice=spice_score)
+    train_pbar.set_postfix(train_loss_gpt=np.mean(train_losses), val_spice=score)
 
-    if args.log: wandb.log({"train_loss_gpt": np.mean(train_losses), "val_spice": spice_score})
+    if args.log: wandb.log({"train_loss_gpt": np.mean(train_losses), "val_spice": score})
 
 print('Finished training decoder')
 if args.log: wandb.finish()
